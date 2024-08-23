d4fdac5930f0968296c910a04159940a468c0d66
Tonie Victor <victornwanochi@gmail.com>
2024-08-23 08:53:44 +0100

    chore: modified some global styles

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -21,6 +21,11 @@ export const RouterHead = component$(() => {
 			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
+			<link rel="stylesheet" href="../../../src/atom-one-dark.css"/>
+			<script src="../../../src/highlight.min.js"></script>
+
+			<script>hljs.highlightAll();</script>
+
 			{head.meta.map((m) => (
 				<meta key={m.key} {...m} />
 			))}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,23 +3,23 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 import styles from "./index.module.css"
 
 export default component$(() => {
-  return (
+	return (
 		<main class={styles.main}>
 			<section class={styles.top__section}>
-				<h1 class={styles.hero__header}>Find, Save and Share your favourite<br/>code snippets for later</h1>
-				<p class={styles.hero__desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec justo in ipsum aliquet consequat.</p>
-				<a href="" class={styles.hero__button}>Explore <i class="fa-solid fa-arrow-right"></i></a>
+				<h1 class={styles.hero__header}>Find, save and share your favourite<br />code snippets for later</h1>
+				<p class={styles.hero__desc}>lorem ipsum dolor sit amet, consectetur adipiscing elit. vestibulum nec justo in ipsum aliquewt consequat.</p>
+				<a href="/snippets" class={styles.hero__button}>explore <i class="fa-solid fa-arrow-right"></i></a>
 			</section>
 		</main>
-  );
+	);
 });
 
 export const head: DocumentHead = {
-  title: "Snipnet",
-  meta: [
-    {
-      name: "Save and interact with code snipnets from the community",
-      content: "Qwik site description",
-    },
-  ],
+	title: "Snipnet",
+	meta: [
+		{
+			name: "Save and interact with code snipnets from the community",
+			content: "Qwik site description",
+		},
+	],
 };

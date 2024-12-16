009d11ed68d6925f01e3fc6b64f6d8936962f139
Tonie Victor <victornwanochi@gmail.com>
2024-12-16 21:56:01 +0100

    removed the optional sharp package

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,6 @@ export default component$(() => {
 		<main class={styles.main}>
 			<section class={styles.top__section}>
 				<h1 class={styles.hero__header}>Find, save and share your favourite<br />code snippets for later</h1>
-				<p class={styles.hero__desc}>lorem ipsum dolor sit amet, consectetur adipiscing elit. vestibulum nec justo in ipsum aliquewt consequat.</p>
 				<a href="/snippets" class={styles.hero__button}>explore <i class="fa-solid fa-arrow-right"></i></a>
 			</section>
 		</main>

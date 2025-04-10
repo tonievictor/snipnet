2cd690fdd9678bb39847b0d13f6d7940ed178aaf
Tonie Victor <victornwanochi@gmail.com>
2025-04-10 11:02:53 +0100

    implemented the navbar

---
+++ components/nav/navbar.tsx
@@ -0,0 +1,44 @@
+import styles from "./nav.module.css"
+import Link from 'next/link'
+import { FaGithub } from "react-icons/fa";
+import Image from 'next/image'
+
+export function Nav() {
+	const loggedin = false;
+	return (
+		<nav className={styles.nav}>
+			<Link href="/" className={styles.nav__logo}>
+				SN<span>.</span>
+			</Link>
+
+			<ul className={styles.nav__list}>
+				<li>
+					<Link href="/">New</Link>
+				</li>
+
+				{!loggedin &&
+					<li className={styles.loginbtn}>
+						<Link href="/" className={styles.loginlink}>
+							<span>Login</span>
+							<FaGithub />
+						</Link>
+					</li>
+				}
+
+
+				{loggedin &&
+					<li>
+						<Image
+							style={{ "border-radius": "50%", "object-fit": "cover", "border": "2px solid var(--light-blue)" }}
+							src="/profile.jpg"
+							width={32}
+							height={32}
+							alt="Picture of the author"
+						/>
+					</li>
+				}
+
+			</ul>
+		</nav>
+	)
+}
